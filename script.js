94e484683c8f350526af267f6fe46e1d8280ff47
= <muriloo.marchezzane@gmail.com>
2025-08-07 10:08:14 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//    // operadores: cosnt, let e var
+//    const nome = 'José'
+//    console.log(nome)
+//    // não vale reatribuir
+//    nome = 'José da Silva'
+//    console.log(nome)
+
+// let nome = 'José'
+// console.log(nome)
+//  console.log(typeof(nome))
+// nome = 'José da Silva'
+//  console.log(nome)
+// nome = 2
+// console.log(nome)
+//  console.log(typeof(nome))
+
+// var nome = 'José'
+// console.log(nome)
+// nome = 'José da Silva'
+//  console.log(nome)
+//  console.log(typeof(nome))
+
+// var linguagem = 'Javascript'
+// console.log("Aprendendo " + linguagem)
+// linguagem = 'Java'
+// console.log("Aprendendo " + linguagem)
+
+let idade = 18
+// hoist = içamento
+console.log('oi, ${nome}')
+if(idade >= 18){
+    let nome = "João"
+    console.log("Você pode dirigir, " + nome)
+}
+console.log("Até mais, " + nome)
